8396e7a2691ea0d7692287188a4d4e20130a59e4
Tan Akpek akpektan@gmail.com <akpektan@gmail.com>
2023-01-27 17:31:19 -0500

    had to remove some files, initiating again

---
+++ backend/routes/user.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { register, activateAccount } = require("../controllers/user");
+
+const router = express.Router();
+
+
+
+router.post("/register", register);
+router.post("/activate", activateAccount);
+
+module.exports = router;
